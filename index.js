cf481eca96e6286fb73b154093ec8cde1a7b83a8
Erik Aakre <erikaakre@hotmail.com>
2023-10-30 22:53:23 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+function createListItem(text, list) {
+  const listItem = document.createElement("li");
+  listItem.innerHTML = `${text}<span class="delete-icon">&#128465;</span>`;
+  list.appendChild(listItem);
+
+  // Add click event to the delete icon
+  const deleteIcon = listItem.querySelector(".delete-icon");
+  deleteIcon.addEventListener("click", () => {
+    listItem.remove();
+  });
+}
+
+const inputElements = Array.from(document.querySelectorAll("input"));
+
+inputElements.forEach((input, index) => {
+  const list = document.getElementById(`list${index + 1}`);
+  input.addEventListener("keydown", (event) => {
+    if (event.key === "Enter" && input.value.trim() !== "") {
+      createListItem(input.value, list);
+      input.value = "";
+    }
+  });
+});
